2807876ce136ac5aca34fcd5d51ea16a755fa492
NChitty <nicholas.chitty@gmail.com>
2020-11-20 16:17:25 -0500

    cleaned up code

--- CheckSum/checksum.java
+++ CheckSum/checksum.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 |   Assignment:  HW 02–Calculating the 8, 16, or 32 bit checksum for a 
 |                given input file
 |
-|    Author:  *********
+|    Author:  Nicholas Chitty
 |    Language:  Java
 |
 |   To Compile:  javac checksum.java checksum
@@ -117,13 +117,12 @@ public class checksum {
 }
 
 /*
- * =============================================================================
- * | I *************** affirm that this program is | entirely my
- * own work and that I have neither developed my code together with | any
- * another person, nor copied any code from any other person, nor permitted | my
- * code to be copied or otherwise used by any other person, nor have I | copied,
- * modified, or otherwise used programs created by others. I acknowledge | that
- * any violation of the above terms will be treated as academic dishonesty.
- * +============================================================================
- * =
+ * +=============================================================================
+ * | I, Wm Nicholas Chitty (wi782067) affirm that this program is 
+ * | entirely my own work and that I have neither developed my code together with 
+ * | any another person, nor copied any code from any other person, nor permitted 
+ * | my code to be copied or otherwise used by any other person, nor have I 
+ * | copied, modified, or otherwise used programs created by others. I acknowledge
+ * | that any violation of the above terms will be treated as academic dishonesty.
+ * +=============================================================================
  */
